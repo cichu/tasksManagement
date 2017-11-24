83e34b6c007f5dcf8d86f878facc286fd8ea01f0
cichu <cichu32@gmail.com>
2017-11-24 01:23:50 +0100

    Wprowadzenie widoku miesiąca w kalendarzu.

--- src/logowanie/LayoutController.java
+++ src/logowanie/LayoutController.java
@@ -12,12 +12,27 @@ public class LayoutController {
     @FXML
     void initialize() {
         this.calendarButton.setOnMouseClicked((event -> {
-            Stage stage = new Stage();
-            stage.setTitle("Kalendarz");
-
-            CalendarView calendarView = new CalendarView();
-            calendarView.setTimeSlotLength(60);
-            calendarView.start(stage);
+            //showCalendarWeekView();
+            showCalendarMonthView();
         }));
+    }
+
+    void showCalendarWeekView() {
+        Stage stage = new Stage();
+        stage.setTitle("Kalendarz");
+
+        CalendarView calendarView = new CalendarView();
+
+        calendarView.setTimeSlotLength(60);
+        calendarView.start(stage);
+    }
+
+    void showCalendarMonthView() {
+        Stage stage = new Stage();
+        stage.setTitle("Kalendarz");
+
+        CalendarMonthView calendarView = new CalendarMonthView();
+
+        calendarView.start(stage);
     }
 }
